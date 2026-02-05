6b5088999f382a7f8fbb516ba2c097971b2872ba
LuisJC-creator <luis.chavez04@utrgv.edu>
2026-02-05 16:58:26 +0000

    1st part of assignment complete

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -1,3 +1,30 @@
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    let newTemp = (f - 32.0) / 1.8;
+    newTemp
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    let newTemp = (c * 1.8) + 30.0;
+    newTemp
+}
+
 fn main() {
-    println!("Hello, world!");
+    // vars we need
+    const frzTemp:f64  = 32.00;
+    let mut usrTemp = 32.00;
+    
+    // looping to convert the next 5 int temps
+    let mut counter = 0;
+    loop {
+        let mut intTemp = usrTemp as i64;
+        println!("{} in celsius is: {}", intTemp, fahrenheit_to_celsius(intTemp as f64));
+
+        if counter == 5 {
+            break;
+        }
+        counter += 1;
+        usrTemp += 1.0;
+        
+    }
+
 }
---
+++ homework1/target/debug/deps/homework1-15aa89988a8c7d83.d
@@ -0,0 +1,5 @@
+/workspaces/spring2026-CS3334/homework1/target/debug/deps/homework1-15aa89988a8c7d83.d: src/main.rs
+
+/workspaces/spring2026-CS3334/homework1/target/debug/deps/homework1-15aa89988a8c7d83: src/main.rs
+
+src/main.rs:
