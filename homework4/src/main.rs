99616b7cdf97cab43b3d45fb55b7e403b2f10b69
LuisJC-creator <luis.chavez04@utrgv.edu>
2026-03-08 21:05:28 +0000

    finished the assignment using the .md file instructions and lines.

--- homework4/src/main.rs
+++ homework4/src/main.rs
@@ -11,10 +11,12 @@ fn perform_operation(operation: FileOperation) {
     match operation {
         FileOperation::Create(filename) => {
             // TODO: Implement file creation logic
+            fs::File::create(&filename).unwrap();
             println!("File '{}' created successfully.", filename);
         }
         FileOperation::Rename(old_name, new_name) => {
             // TODO: Implement file renaming logic
+            fs::rename(&old_name, &new_name).unwrap();
             println!("File renamed from '{}' to '{}' successfully.", old_name, new_name);
         }
     }
@@ -31,10 +33,22 @@ fn main() {
     match choice.trim() {
         "1" => {
             // TODO: Prompt for new filename and call perform_operation
+            println!("Enter a new name: ");
+            let mut input_string = String::new();
+            io::stdin().read_line(&mut input_string).unwrap();
+            perform_operation(FileOperation::Create(input_string.trim().to_string()));
         }
         "2" => {
             // TODO: Prompt for old and new filenames and call perform_operation
-        }
+            println!("Enter old file name: ");
+            let mut old_name = String::new();
+            io::stdin().read_line(&mut old_name).unwrap();
+            println!("Enter new file name: ");
+            let mut new_name = String::new();
+            io::stdin().read_line(&mut new_name).unwrap();
+            perform_operation(FileOperation::Rename(old_name.trim().to_string(), new_name.trim().to_string()));
+
+        }   
         _ => println!("Invalid choice"),
     }
 }
